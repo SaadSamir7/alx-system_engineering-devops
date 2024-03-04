f88dc5a2948ecea501a569f4b939e5aa68cc21c7
SaadSamir7 <saadsamir7177@gmail.com>
2024-03-04 19:40:47 +0200

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #Client configuration file (w/ Puppet)
 
 lines = 'Host 481773-web-01
-    HostName 100.25.179.4
+    HostName 107.23.94.249
     User ubuntu
     IdentifyFile ~/.ssh/school
     PasswordAuthentication no'
